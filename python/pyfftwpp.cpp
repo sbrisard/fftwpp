f8a413b64244d3df78d4d5a07582fe60362c595e
Sébastien Brisard <sbrisard@users.noreply.github.com>
2021-03-12 03:10:20 +0100

    Use new to avoid copies of Plan

--- python/pyfftwpp.cpp
+++ python/pyfftwpp.cpp
@@ -13,18 +13,18 @@ PYBIND11_MODULE(pyfftwpp, m) {
 
   pybind11::class_<Plan>(m, "Plan")
       .def(pybind11::init([](array in, array out) {
-        pybind11::buffer_info info_in = in.request();
-        pybind11::buffer_info info_out = out.request();
-        if ((info_in.ndim != 1) || (info_out.ndim != 1)) {
-          throw std::runtime_error("expected one dimensional arrays");
-        }
-        if (info_out.size < info_in.size) {
-          throw std::runtime_error(
-              "output array must be larger than input array");
-        }
-        int size = info_in.size;
-        return Plan{size, in.mutable_data(), out.mutable_data()};
-      }))
+             pybind11::buffer_info info_in = in.request();
+             pybind11::buffer_info info_out = out.request();
+             if ((info_in.ndim != 1) || (info_out.ndim != 1)) {
+               throw std::runtime_error("expected one dimensional arrays");
+             }
+             if (info_out.size < info_in.size) {
+               throw std::runtime_error(
+                   "output array must be larger than input array");
+             }
+             int size = info_in.size;
+             return new Plan{size, in.mutable_data(), out.mutable_data()};
+           }))
       .def("execute", &Plan::execute)
       .def("cost", &Plan::cost)
       .def("flops", &Plan::flops)
